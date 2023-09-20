47fcaa318e60306abd5c82daef44c504e9f03ab3
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-09-20 15:01:22 +0200

    Added About Content

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import '../about/about.scss'
 import '../navbar/navbar'
+import AboutImg from '../../assets/about.jpg'
 
 function About() {
   return (
     <>
       <section id='about'>
-        this is a test
+      <h3 id='headline'> <span id='headlineDiff'>EXQUISITE</span> APPROACH TO CUSTOMERS </h3>
+      <br /><br />
+        <div id='aboutBlock'>
+          <br /><br /><br /><br />
+          <div id='aboutImgContainer'>
+            <img src={AboutImg} id='aboutImg' />
+          </div>
+          <div id="aboutText">
+            Raven Studio is a tattoo parlor which provides all kinds of tattooing & piercing services. Our skilled specialists will provide a free consultation and will offer a variety of bestseller tattoo designs, or you may ask for any kind of custom design. <br /><br /> Go ahead and book yourself and appointment today!
+            <br /><br />
+            <h3>Read More</h3>
+            <br />
+            <button id='aboutBtn'> - </button>
+          </div>
+        </div>
       </section>
     </>
   )
