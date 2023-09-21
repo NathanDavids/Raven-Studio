c770d0d922b670228a12d1b3807c95fe6cd75126
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-09-21 10:43:24 +0200

    Worked on Styling (Home)

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import '../home/home.scss'
 import '../navbar/navbar'
-import {GiBranchArrow} from 'react-icons/gi'
 
 function Home() {
   return (
@@ -10,7 +9,7 @@ function Home() {
         <div id='overlay'></div>
         <div id='homeContainer'>
           <div id='textContainer'>
-          <h1 id='headline'><span id='headlineColor'> Express </span> Yourself</h1>
+          <h1 id='homeHeadline'><span id='homeHeadlineColor'> Express </span> Yourself</h1>
             <h3 id='slogan'>UNLEASH A NEW <span id='sloganDiff'> DIMENSION </span> OF ART </h3>
             <button id='bookingBtn'> Book Appointment </button>
           </div>
