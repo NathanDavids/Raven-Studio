89d8485553c59dfb63ac68a1e0c605276c6bf71e
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-09-21 19:23:52 +0200

    Added carousel function to artist info (Artists)

--- src/components/artists/artists.jsx
+++ src/components/artists/artists.jsx
@@ -1,13 +1,70 @@
-import React from 'react'
-import '../artists/artists.scss'
-import AboutImg from '../../assets/about.jpg'
-import {BsArrowRight} from 'react-icons/bs'
+import React, { useState } from 'react';
+import '../artists/artists.scss';
+import AboutImg from '../../assets/about.jpg';
+import { BsArrowRight } from 'react-icons/bs';
+
+const artistsData = [
+  {
+    name: 'Jane Davies',
+    title: 'Tattoo Artist',
+    experience: '7 Years',
+    style: 'Realism, Portraiture, Classic',
+    ranking: 'Senior',
+    description:
+      'Jane says "Imagine a beauty and keep it with you". She specializes in realism style and always tries to get the best end product.',
+  },
+  {
+    name: 'John Smith',
+    title: 'Tattoo Artist',
+    experience: '5 Years',
+    style: 'Traditional, Japanese',
+    ranking: 'Intermediate',
+    description:
+      'John is passionate about traditional tattooing and has a unique style that combines elements of Japanese tattoo art.',
+  },
+  {
+    name: 'Emily Black',
+    title: 'Tattoo Artist',
+    experience: '9 Years',
+    style: 'Watercolor, Abstract, Geometric',
+    ranking: 'Master',
+    description:
+      'Emily is known for her avant-garde approach to tattoo art, blending watercolor techniques with abstract and geometric designs for stunning and unique tattoos.'
+  },
+  {
+    name: 'Michael Greene',
+    title: 'Tattoo Artist',
+    experience: '12 Years',
+    style: 'Neo-Traditional, New School, Surrealism',
+    ranking: 'Expert',
+    description:
+      'Michael is a versatile artist who excels in various styles, from Neo-Traditional to Surrealism. He believes that tattoos should tell a story and bring imagination to life.'
+  },
+  {
+    name: 'Sarah Walker',
+    title: 'Tattoo Artist',
+    experience: '8 Years',
+    style: 'Black and Gray, Fine Line, Script',
+    ranking: 'Senior',
+    description:
+      'Sarah specializes in creating elegant and intricate black and gray tattoos. Her attention to detail and skill with fine line work make her a sought-after artist for script and delicate designs.'
+  },
+  // Add more artists as needed
+];
 
 function Artists() {
+  const [currentArtistIndex, setCurrentArtistIndex] = useState(0);
+
+  const nextArtist = () => {
+    setCurrentArtistIndex((prevIndex) => (prevIndex + 1) % artistsData.length);
+  };
+
+  const artist = artistsData[currentArtistIndex];
+
   return (
     <>
       <section id='artists'>
-        <h3 id='artistsHeadline'> STATS WE <span id='artistsDiff'> BRAG </span> ABOUT </h3>
+      <h3 id='artistsHeadline'> STATS WE <span id='artistsDiff'> BRAG </span> ABOUT </h3>
         <br /><br />
         <hr id='statsline1'/>
         <div id='stats'>
@@ -36,44 +93,45 @@ function Artists() {
         <hr id='statsline1'/>
         <br /><br /><br />
         <h3 id='artistsHeadline'> <span id='artistsDiff'> ARTISTS </span> WE'RE PROUD OF </h3>
-        <div id='artistsContainer'>
-          <div id='artistInfo'>
-            <div id='top'>
-            <h2 id='nameTag'> Jane Davies </h2>
-            
-            <div id='title'>
-              <h4>Tattoo Artist</h4>
-            </div>
-            </div>
-            <div id='middle'>
-            <h3 id='artistText'>Experience <span id='middleLine'> | </span> 7 Years </h3>
-            <br />
-            <h3 id='artistText'>Style <span id='middleLine'> | </span> Realism, Portraiture, Classic </h3>
-            <br />
-            <h3 id='artistText'>Ranking <span id='middleLine'> | </span> Senior </h3>
-            </div>
-            <hr />
-            <div id='bottom'>
-              <div id='bottomText'>
-                <h5>
-                  Jane says "Imagine a beauty and keep it with you". She specializes in realism style and always tries to get the best end product.
-                </h5>
+        {artistsData.map((artist, index) => (
+          <div key={index} id='artistsContainer' style={{ display: index === currentArtistIndex ? 'flex' : 'none' }}>
+            <div id='artistInfo'>
+              <div id='top'>
+                <h2 id='nameTag'>{artist.name}</h2>
+                <div id='title'>
+                  <h4>{artist.title}</h4>
+                </div>
+              </div>
+              <div id='middle'>
+                <h3 id='artistText'>Experience <span id='middleLine'> | </span> {artist.experience}</h3>
+                <br />
+                <h3 id='artistText'>Style <span id='middleLine'> | </span> {artist.style}</h3>
+                <br />
+                <h3 id='artistText'>Ranking <span id='middleLine'> | </span> {artist.ranking}</h3>
+              </div>
+              <hr />
+              <div id='bottom'>
+                <div id='bottomText'>
+                  <h5>{artist.description}</h5>
+                </div>
               </div>
             </div>
+            <div id='artistImgContainer'>
+              <img src={AboutImg} id='artistImg' alt='' />
+            </div>
           </div>
-          <div id='artistImg'>
-            <img src={AboutImg} alt="" />
-          </div>
-        </div>
+        ))}
         <br />
         <hr />
         <div id='slideContainer'>
-          <button id='slideBtn'> <BsArrowRight id='nextIcon' /> </button>
+          <button id='slideBtn' onClick={nextArtist}>
+            <BsArrowRight id='nextIcon' />
+          </button>
         </div>
         <hr />
       </section>
     </>
-  )
+  );
 }
 
-export default Artists
+export default Artists;
