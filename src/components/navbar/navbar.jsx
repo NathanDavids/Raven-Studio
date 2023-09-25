eb9e4bb7446493acde640e97835e0c6f7b376fef
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-09-25 13:51:53 +0200

    Added page links to the menu (Navbar)

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -3,15 +3,15 @@ import '../navbar/navbar.scss';
 import { BiMenu } from 'react-icons/bi';
 
 function Navbar() {
-  // Step 1: Create a state variable to track menu visibility
+  // a state variable to track menu visibility
   const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-  // Step 2: Create a function to toggle menu visibility
+  // function to toggle menu visibility
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  // Step 3: Create a function to close the menu
+  // function to close the menu
   const closeMenu = () => {
     setIsMenuOpen(false);
   };
@@ -32,23 +32,19 @@ function Navbar() {
         </nav>
       </section>
 
-      {/* Step 2: Create a modal overlay */}
       {isMenuOpen && (
         <div className='modal-overlay'>
           <div className='menu-container'>
-            {/* Add a close button to the menu */}
             <button className='close-button' onClick={closeMenu}>
               Close
             </button>
-            {/* Add your menu items here */}
             <ul id='menu-items'>
-              <a href=""><li>HOME</li></a>
-              <a href=""><li>ABOUT</li></a>
-              <a href=""><li>SOLUTIONS</li></a>
-              <a href=""><li>ARTISTS </li></a>
-              <a href=""><li>FAQ</li></a>
-              <a href=""><li>BOOKINGS</li></a>
-              {/* Add more items as needed */}
+              <a href="#home"><li>HOME</li></a>
+              <a href="#about"><li>ABOUT</li></a>
+              <a href="#solutions"><li>SOLUTIONS</li></a>
+              <a href="#artists"><li>ARTISTS </li></a>
+              <a href="#faqSection"><li>FAQ</li></a>
+              <a href="#booking"><li>BOOKINGS</li></a>
             </ul>
           </div>
         </div>
